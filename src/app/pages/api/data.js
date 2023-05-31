340c4914fac2cbc97486c312e41a66ec180ca778
hejvalli <valentin.okutan@gmail.com>
2023-05-31 22:49:13 +0300

    serverside api calls

--- src/app/pages/api/data.js
+++ src/app/pages/api/data.js
@@ -1,34 +1,42 @@
- export async function getData() {
-    const res = await fetch('http://164.92.130.40:3000/data', {
-      headers: {
-        'Authorization': 'Basic ' + btoa('root:kArotte11!k')
-      }
-    });
-    // The return value is *not* serialized
-    // You can return Date, Map, Set, etc.
-   
-    // Recommendation: handle errors
-    if (!res.ok) {
-      // This will activate the closest `error.js` Error Boundary
-      throw new Error('Failed to fetch data');
+export async function getServerSideProps() {
+  const res = await fetch('http://164.92.130.40:3000/data', {
+    headers: {
+      'Authorization': 'Basic ' + btoa('root:kArotte11!k')
     }
-   
-    return res.json();
+  });
+  // The return value is *not* serialized
+  // You can return Date, Map, Set, etc.
+
+  // Recommendation: handle errors
+  if (!res.ok) {
+    // This will activate the closest `error.js` Error Boundary
+    throw new Error('Failed to fetch data');
   }
-  export async function postData(data) {
-    const res = await fetch('http://164.92.130.40:3000/data', {
-      method: 'PATCH',
-      headers: {
-        'Authorization': 'Basic ' + btoa('root:kArotte11!k'),
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(data),
-    });
-  
-    // Recommendation: handle errors
-    if (!res.ok) {
-      throw new Error('Failed to update data');
-    }
-  
-    return res.json();
-  };
+
+  const data = await res.json();
+  return {
+    props: { data }
+  }
+}
+
+export default async function postData(req, res) {
+  const data = req.body;
+
+  const response = await fetch('http://164.92.130.40:3000/data', {
+    method: 'PATCH',
+    headers: {
+      'Authorization': 'Basic ' + btoa('root:kArotte11!k'),
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+
+  if (!response.ok) {
+    throw new Error('Failed to update data');
+  }
+
+  const responseData = await response.json();
+
+  res.status(200).json(responseData);
+}
+
